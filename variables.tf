3d18f53f4b27763c914910ea5ac8013d30ff4c78
gobimages <32836630+gobimages@users.noreply.github.com>
2018-12-31 14:20:53 +0400

    commit ordered configuration for variables

--- variables.tf
+++ variables.tf
@@ -1,35 +1,15 @@
-variable "Client_Secret" {
-}
-variable "Client_ID" {
-  
-}
-variable "Subscription_ID" {
-  
-}
-variable "Tenant_ID" {
-  
-}
-variable "Resource_Group" {
-  
-}
+variable "Client_Secret" {}
+variable "Client_ID" {}
+variable "Subscription_ID" {}
+variable "Tenant_ID" {}
+variable "Resource_Group" {}
 variable "Location" {
     type = "string"
     default = "northeurope"
-  
-}
-variable "nic_name" {
-  
-}
-variable "IP_confname" {
-  
-}
-variable "Storage_AccountName" {
-  
-}
-variable "VirtualMachine_Name" {
-  
-}
-variable "VM_Size" {
-  
-}
+  }
+variable "nic_name" {}
+variable "IP_confname" {}
+variable "Storage_AccountName" {}
+variable "VirtualMachine_Name" {}
+variable "VM_Size" {}
 
